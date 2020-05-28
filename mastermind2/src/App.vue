8844188f00092d5514532a2dd50d7ed098782ece
Robinson Batista Madrigal <46830749+robinson1919@users.noreply.github.com>
2020-05-28 06:21:18 +0100

    first commit

---
+++ mastermind2/src/App.vue
@@ -0,0 +1,152 @@
+<template>
+  <v-app id="inspire">
+    <v-container grid-list-xs>
+      <v-card>
+        <v-card-title ml-5 primary-title class="headline">
+          MasterMind
+        </v-card-title>
+
+        <v-row cols="12" sm="4" md="4" class="mar">
+          <v-col sm="1" md="1">
+            <v-text-field
+              solo
+              v-on:click="dialog = true"
+            ></v-text-field>
+          </v-col>
+
+          <v-col sm="1" md="1">
+            <v-text-field
+              solo
+              v-on:click="dialog = true"
+            ></v-text-field>
+          </v-col>
+
+          <v-col sm="1" md="1">
+            <v-text-field
+              solo
+              v-on:click="dialog = true"
+            ></v-text-field>
+          </v-col>
+
+          <v-col sm="1" md="1" class="mr-5">
+            <v-text-field
+              solo
+              v-on:click="dialog = true"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+
+        <v-row cols="8" sm="6" md="3">
+          <hello-world @modalStatus="modal" :selectedColor="color"></hello-world>
+        </v-row>
+      </v-card>
+
+      <!-- modal start -->
+      
+      <v-dialog
+        v-model="dialog"
+        width="350"
+      >
+        <v-card>
+          <v-card-title
+            class="headline grey lighten-2"
+            primary-title
+          >
+            Pick a color
+          </v-card-title>
+
+          <v-row class="col-md-12 mx-auto">
+            <input type="text" class="col-md-3 blue hv" @click="getColor(1)">
+            <input type="text" class="col-md-3 red hv" @click="getColor(2)">
+            <input type="text" class="col-md-3 yellow hv" @click="getColor(3)">
+          </v-row>
+          <v-row class="col-md-12 mx-auto">
+            <input type="text" class="col-md-3 green hv" @click="getColor(4)">
+            <input type="text" class="col-md-3 white hv" @click="getColor(5)">
+            <input type="text" class="col-md-3 black hv" @click="getColor(6)">
+          </v-row>
+
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              accept
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <!-- modal end -->
+    
+    
+    </v-container>
+  </v-app>  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    dialog: false,
+    color: '',
+  }),
+
+  methods: {
+
+    modal(arg){
+      this.dialog = arg;
+    },
+
+    getColor(color){
+      this.color = color;
+    }
+  },
+};
+</script>
+
+
+<style  scoped>
+
+.mar {
+  margin-left: 1rem;
+}
+
+.blue {
+  background-color: blue;
+}
+
+.red {
+  background-color: red;
+}
+
+.white {
+  background-color: white;
+}
+
+.yellow {
+  background-color: yellow;
+}
+
+.green {
+  background-color: green;
+}
+
+.black {
+  background-color: black;
+}
+
+.hv {
+  cursor: pointer;
+}
+
+</style>
---
+++ mastermind2/src/components/HelloWorld.vue
@@ -0,0 +1,209 @@
+<template>
+  <v-container>
+    <v-row sm="12" md="12" class="mar" v-for="(casilla, index) in casillas" :key="index">
+
+      <v-col sm="1" md="1">
+        <v-text-field
+          solo
+          v-model="casilla.sel1"
+          :class="[casilla.sel1]"
+          v-on:click="sendToP(true)"
+          @click="selected('sel1', index)"
+        ></v-text-field>
+      </v-col>
+
+      <v-col sm="1" md="1">
+         <v-text-field
+          solo
+          v-model="casilla.sel2"
+          :class="[casilla.sel2]"
+          v-on:click="sendToP(true)"
+          @click="selected('sel2', index)"
+        ></v-text-field>
+      </v-col>
+
+      <v-col sm="1" md="1">
+         <v-text-field
+          solo
+          v-model="casilla.sel3"
+          :class="[casilla.sel3]"
+          v-on:click="sendToP(true)"
+          @click="selected('sel3', index)"
+        ></v-text-field>
+      </v-col>
+
+      <v-col sm="1" md="1" class="mr-5">
+         <v-text-field
+          solo
+          v-model="casilla.sel4"
+          :class="[casilla.sel4]"
+          v-on:click="sendToP(true)"
+          @click="selected('sel4', index)"
+        ></v-text-field>
+      </v-col>
+
+      <!--  -->
+       <v-divider
+          class="mx-4 divider"
+          vertical
+        ></v-divider>
+      <!--  -->
+
+      <!-- hints -->
+      <v-col sm="1" md="1">
+        <v-text-field
+          solo
+          :class="[cHint1]"
+        ></v-text-field>
+      </v-col>
+      <v-col sm="1" md="1">
+        <v-text-field
+          solo
+          :class="[cHint2]"
+        ></v-text-field>
+      </v-col>
+      <v-col sm="1" md="1">
+        <v-text-field
+          solo
+          :class="[cHint3]"
+        ></v-text-field>
+      </v-col>
+      <v-col sm="1" md="1">
+        <v-text-field
+          solo
+          :class="[cHint4]"
+        ></v-text-field>
+      </v-col>
+
+      <!-- Button -->
+      <v-col sm="1" md="1">
+        <v-btn color="success" @click="CheckNadd">
+          
+          Set
+        </v-btn>
+      </v-col>
+      <!-- Button -->
+      {{selectedColor}}
+      <!-- hints end -->
+
+
+
+    
+      
+     
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',  
+    props: {
+      selectedColor: {}
+    },  
+    data: () => ({
+      cHint1: '',
+      cHint2: '',
+      cHint3: '',
+      cHint4: '',
+      selection: {},
+      casillas: [
+        {
+          sel1: '',
+          sel2: '',
+          sel3: '',
+          sel4: '',
+          hint1: '',
+          hint2: '',
+          hint3: '',
+          hint4: '',
+        }
+      ]
+    }),
+    created(){
+      
+    },
+
+    
+
+    methods: {
+      sendToP(val){
+        var dialog = val
+        this.$emit('modalStatus', dialog);
+      },
+
+      CheckNadd(){
+        this.casillas.push(
+          {
+            sel1: '',
+            sel2: '',
+            sel3: '',
+            sel4: '',
+            hint1: '',
+            hint2: '',
+            hint3: '',
+            hint4: '',
+          }
+        )
+      },
+      selected(val, index){
+        this.selection = { cel: val, position: index}
+      },
+
+      addColor(selectedColor){
+        var index = "";
+        var cel = "";
+        index = this.selection.position;
+        cel = this.selection.cel;
+
+        switch(selectedColor){
+          case 1:
+            this.casillas[index][cel] = 'blue';
+            break;
+
+          case 2:
+            this.casillas[index][cel] = 'red';
+            break;
+
+          case 3:
+            this.casillas[index][cel] = 'yellow';
+            break;
+
+          case 4:
+            this.casillas[index][cel] = 'green';
+            break;
+
+          case 5:
+            this.casillas[index][cel] = 'white';
+            break;
+
+          case 6:
+            this.casillas[index][cel] = 'black';
+            break;
+        }
+      }
+    },
+    watch: {
+      'selectedColor'(){
+        this.addColor(this.selectedColor);
+      }
+    },
+
+    
+  }
+</script>
+
+
+<style scoped>
+
+.divider {
+  margin-right: 5rem;
+  margin-left: 1rem;
+}
+
+.mar {
+  margin-left: 1rem;
+}
+
+</style>
